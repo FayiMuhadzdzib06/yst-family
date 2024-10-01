50060dade3cb6159a23c1cb5f5fdeb7e939b13d9
Fayi Muhadzdzib <fayimujahid1@gmail.com>
2024-10-01 14:14:52 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <script lang="ts" setup>
   import Navbar from '@/components/Navbar.vue';
   import Home from '@/components/Home.vue';
-  import About from '~/components/About.vue';
-  import Services from '@/components/Services.vue';
-  import Cabang from '@/components/Cabang.vue';
-  import ViewApk from '@/components/ViewApk.vue';
-  import Footer from '@/components/Footer.vue';
+  const About = defineAsyncComponent(() => import('~/components/About.vue'));
+  const Services = defineAsyncComponent(() => import('~/components/Services.vue'));
+  const Cabang = defineAsyncComponent(() => import('~/components/Cabang.vue'));
+  const ViewApk = defineAsyncComponent(() => import('~/components/ViewApk.vue'));
+  const Footer = defineAsyncComponent(() => import('~/components/Footer.vue'));
 </script>
 
 <template>
