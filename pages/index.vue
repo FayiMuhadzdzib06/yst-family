61f2015257836e0aa0c388109d7034c913bef4cc
Fayi Muhadzdzib <fayimujahid1@gmail.com>
2024-10-01 10:37:29 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
   import Navbar from '@/components/Navbar.vue';
   import Home from '@/components/Home.vue';
-  import About from '@/components/About.vue';
+  import About from '~/components/About.vue';
   import Services from '@/components/Services.vue';
   import Cabang from '@/components/Cabang.vue';
   import ViewApk from '@/components/ViewApk.vue';
